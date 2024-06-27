87bc206b462aa0290243c0e55c32ffa7fd937cc4
borishdez <borisnandez93@gmail.com>
2024-06-26 18:43:49 -0600

    Nueva Vista Contacto y redireccion

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Empresa_Form_Contact.Models;
+using System.Threading.Tasks;
+
+namespace Empresa_Form_Contact.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly MueblescorpContext _context;
+
+        public ContactsController(MueblescorpContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubmitContactForm([Bind("Nombre,Correo,Mensaje")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contact);
+                await _context.SaveChangesAsync();
+                TempData["Message"] = "Contacto enviado con éxito";
+                return RedirectToAction(nameof(Index));
+            }
+            return View("Index", contact);
+        }
+    }
+}
